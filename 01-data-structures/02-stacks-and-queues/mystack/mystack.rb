86a4f4d41a717fa307e70be76b2be2efce41886c
Muhammad <daudjaved1@gmail.com>
2018-01-17 21:52:52 +0000

    assignment completed

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,21 @@ class MyStack
   end
 
   def push(item)
+    
+   @stack.unshift(item)
+   self.top = @stack[0]
   end
 
   def pop
+    self.top = @stack[1]
+    @stack.shift
   end
 
   def empty?
+    if @stack.empty?
+      return true
+    else
+      return false
+    end
   end
 end
