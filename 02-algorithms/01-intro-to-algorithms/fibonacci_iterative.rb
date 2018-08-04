a01de4953fbe780de6167684e13026cd872c999b
Muhammad <daudjaved1@gmail.com>
2018-08-04 08:11:03 +0000

    fib fix

--- 02-algorithms/01-intro-to-algorithms/fibonacci_iterative.rb
+++ 02-algorithms/01-intro-to-algorithms/fibonacci_iterative.rb
@@ -2,10 +2,14 @@ def fib(n)
   fib_0 = 0
   fib_1 = 1
   range = (0..(n-1))
-  range.each do |ele|
-      ele = fib_0
-      fib_0 = fib_1 
-      fib_1 = ele + fib_1 
+  if n == 0 
+      return 0
+  else
+    range.each do |ele|
+        ele = fib_0
+        fib_0 = fib_1 
+        fib_1 = ele + fib_1 
+    end
   end
   return fib_1
 end
