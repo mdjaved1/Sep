4f98fdcb8bb06702b31177baa0f36094c63958cd
Muhammad <daudjaved1@gmail.com>
2018-08-20 08:23:27 +0000

    completed intro complexity

---
+++ 03-complexity/01-complexity-intro/answer-8.rb
@@ -0,0 +1,15 @@
+def Big_O(n)
+    "#{n} = worst case number of iterations "
+end
+
+
+puts Big_O(1)
+puts Big_O(2)
+puts Big_O(3)
+puts Big_O(4)
+puts Big_O(5)
+puts Big_O(6)
+puts Big_O(7)
+puts Big_O(8)
+puts Big_O(9)
+puts Big_O(10)
